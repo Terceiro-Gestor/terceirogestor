78879dd62bef8fc9f7d79df55870390b69089e99
wevertoncamposdev <wevertoncamposdev@gmail.com>
2025-05-09 12:15:43 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 return [
     '/' => function () {
-        echo "Página inicial do Terceiro Gestor!";
+        echo "Bem-vindo ao Terceiro Gestor!";
     },
     '/sobre' => function () {
         echo "Sobre o Terceiro Gestor.";
