222eec0a5f5ab71f7e4130c25173d33049540244
wevertoncamposdev <wevertoncamposdev@gmail.com>
2025-05-09 15:58:58 -0300

    update page home

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Controllers\MainController;
 use App\Middleware\Middleware;
 
 Route::get('/', function () {
-    echo "Bem-vindo ao Terceiro Gestor!";
+    require_once __DIR__ . '/../app/Views/page/home.html';
 });
 
 Route::get('/sobre', function () {
@@ -16,6 +16,7 @@ Route::get('/sobre', function () {
 Route::get('/entrar', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/sair', [AuthController::class, 'logout']);
+
 // Rotas protegidas por middleware
 Route::get('/painel', function () {
     Middleware::auth();
