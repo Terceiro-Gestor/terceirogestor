1c03dbe7907ad4241529b0baed87f3ea9056a713
wevertoncamposdev <wevertoncamposdev@gmail.com>
2025-05-09 11:21:55 -0300

    estruturação

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Bem-vindo ao Terceiro Gestor!";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$routes = require __DIR__ . '/../routes/web.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (array_key_exists($uri, $routes)) {
+    $routes[$uri]();
+} else {
+    http_response_code(404);
+    echo "Página não encontrada.";
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '/' => function () {
+        echo "Página inicial do Terceiro Gestor!";
+    },
+    '/sobre' => function () {
+        echo "Sobre o Terceiro Gestor.";
+    },
+];
